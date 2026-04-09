75632060082cb2f142a942bd51e18b681da6c10a
dfamrln <mhdffa1@gmail.com>
2026-04-09 15:29:20 +0700

    penambahan private void ClearForm

--- CRUDMahasiswaADO/Form1.cs
+++ CRUDMahasiswaADO/Form1.cs
@@ -278,6 +278,16 @@ namespace CRUDMahasiswaADO
         // ===============================
         // CLEAR FORM
         // ===============================
+        private void ClearForm()
+        {
+            txtNIM.Clear();
+            txtNama.Clear();
+            cmbJK.SelectedIndex = -1;
+            txtAlamat.Clear();
+            txtKodeProdi.Clear();
+            dtpTanggalLahir.Value = DateTime.Now;
+            txtNIM.Focus();
+        }
 
         // ===============================
         // FORM LOAD
